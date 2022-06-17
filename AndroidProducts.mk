d30c45223e14f98b71cb3bf3b6b24ea5b123b3ac
Imtiaz Modder <67188507+Imtiaz-Modder@users.noreply.github.com>
2022-06-17 12:31:29 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/blaze_merlinx.mk
+   $(LOCAL_DIR)/aosp_merlinx.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_merlinx-user \
-    blaze_merlinx-userdebug \
-    blaze_merlinx-eng
+    aosp_merlinx-user \
+    aosp_merlinx-userdebug \
+    aosp_merlinx-eng
