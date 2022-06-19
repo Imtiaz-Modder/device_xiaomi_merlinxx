274f4de2dadddbe651186eb34f279a8ce11bf90f
Imtiaz Modder <67188507+Imtiaz-Modder@users.noreply.github.com>
2022-06-19 19:41:21 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/bliss_merlinx.mk
+   $(LOCAL_DIR)/corvus_merlinx.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_merlinx-user \
-    bliss_merlinx-userdebug \
-    bliss_merlinx-eng
+    corvus_merlinx-user \
+    corvus_merlinx-userdebug \
+    corvus_merlinx-eng
