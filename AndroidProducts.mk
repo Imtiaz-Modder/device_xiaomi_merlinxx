6e5ebe704ba84ef4ab5f032131c63126a8976b73
Imtiaz Modder <67188507+Imtiaz-Modder@users.noreply.github.com>
2022-06-22 06:33:38 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/voltage_merlinx.mk
+   $(LOCAL_DIR)/corvus_merlinx.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_merlinx-user \
-    voltage_merlinx-userdebug \
-    voltage_merlinx-eng
+    corvus_merlinx-user \
+    corvus_merlinx-userdebug \
+    corvus_merlinx-eng
